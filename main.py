60a93030ceeff1f29e18043123d17e468fcc033d
phamtungthuy <94853261+phamtungthuy@users.noreply.github.com>
2023-03-08 00:18:28 +0700

    update from pc

--- main.py
+++ main.py
@@ -7,26 +7,46 @@ s.connect((ip, port))
 
 s.send(struct.pack('<ii8s', 0, 8, "21020412".encode()))
 
+def powMod(a, n, m):
+    if n <= 0: return 1
+    res = powMod(a, n // 2, m) % m
+    if n % 2 == 1: return ((res * res) % m * (a % m)) % m
+    return (res * res) % m
 while True:
-    packet = s.recv(1024)
+    packet = s.recv(2048)
+    print('next')
     if len(packet) < 8 : continue
     pk_type, pk_len = struct.unpack('<ii', packet[:8])
     print(pk_type, pk_len)
     if pk_type == 0:
         s.send(struct.pack('<ii8s', 0, 8, "21020412".encode()))
     elif pk_type == 1:
-        len_int = (pk_len - 8) / 4
+        len_int = int((pk_len - 8) / 4)
         format = '<' + str(len_int) + 'i';
-        print(format)
-        date_list = struct.unpack(format, packet[8:pk_len])
+        data_list = list(struct.unpack(format, packet[8:pk_len]))
         n, m, x = data_list[:3]
-        ans = data_list[3]
-        print(n, m, x)
-        for i in range(4, len(date_list)):
-            print(i)
-        break
+        ans = 0
+        # tmp = 0
+        # for i in range(3, 5):
+        #     tmp += ((data_list[i]%m) * ((x ** (i - 3)) % m))%m
+        #     tmp %= m
+        #     print(m);
+        print(f'm = {m}')
+        print(f'x ={x}')
+        # print('2 so dau: ', data_list[3], data_list[4], tmp)
+        
+        for i in range(3, len(data_list)):
+            ans += ((data_list[i]%m) * powMod(x, i - 3, m)) % m
+            ans %= m
+        print(ans)
+        # print(format);
+        # print(ans)
+        s.send(struct.pack('<iii', 2, 4, ans))
     elif pk_type == 3:
         s.close()
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((ip, port))
         s.send(struct.pack('<ii8s', 0, 8, "21020412".encode()))
+    elif pk_type == 4:
+        print(packet[8:8+pk_len].decode())
+        break
